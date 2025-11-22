5c728184da082026319c37629035103e113b6e3f
DangKhoaNguyenLe <nguyenledangkhoa146205@gmail.com>
2025-11-22 16:31:14 +0700

    update

--- DAL/PhongTroDAL.cs
+++ DAL/PhongTroDAL.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DTO;
 namespace DAL
 {
     public class PhongTroDAL : DbProcessDAL
     {
+        public List<PhongTroDTO> getList()
+        {
+            List<PhongTroDTO> phongTros = new List<PhongTroDTO>();
+            DataTable dt = room.GetData();
 
+            foreach (DataRow dr in dt.Rows)
+            {
+                DataNhaTro.ROOMRow ht = dr as DataNhaTro.ROOMRow;
+                PhongTroDTO d = new PhongTroDTO(
+                    ht.RoomID,
+                    ht.HostelID,
+                    ht.RoomName,
+                    ht.Floor,
+                    ht.Area,
+                    ht.Capacity,
+                    ht.soNguoiDaThue,
+                    ht.Price,
+                    ht.Deposit,
+                    ht.Status);
+                phongTros.Add(d);
+            }
+            return phongTros;
+        }
     }
 }
